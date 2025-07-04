651a6344987e78a4e8c1814952b27fbdbbe505db
agilimam <agilimamalhafizi04@gmail.com>
2025-07-04 21:40:15 +0700

    Perbaikan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -14,17 +15,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Seeder untuk user admin
+        // User::factory(10)->create();
+
         User::factory()->create([
             'name' => 'AdminKoi',
             'email' => 'adminkoi@mail.com',
             'email_verified_at' => now(),
             'password' => Hash::make('kelompok4'),
             'remember_token' => Str::random(10),
-            'role' => 'admin', // ganti is_admin
+            'role' => 'admin', //menggantikan is_admin
         ]);
-
-        // Tambahkan ProductSeeder
-        $this->call(ProductSeeder::class);
     }
 }
